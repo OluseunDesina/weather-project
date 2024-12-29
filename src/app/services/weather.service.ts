5da40a2874e74d04d66611c5216a77d40a27b8a7
oluseunDesina <desinaoluseun@gmail.com>
2024-12-29 06:31:31 +0100

    using environment variables

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
@@ -9,15 +10,16 @@ export class WeatherService {
   constructor() {}
 
   private http = inject(HttpClient);
+  private apiKey = environment.apiKey;
 
   getWeatherData(city: string): Observable<any> {
     return this.http.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=dae5ad3fe83c4285b49ccea13b342f71`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${this.apiKey}`
     );
   }
   getForecastData(city: string): Observable<any> {
     return this.http.get(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=Metric&cnt=5&appid=dae5ad3fe83c4285b49ccea13b342f71`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=Metric&cnt=5&appid=${this.apiKey}`
     );
   }
 }
